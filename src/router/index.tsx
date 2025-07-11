0d1d4897f63ba0d9ee24847125be21046810f88e
Le Duc Thai <leducthai1008@gmail.com>
2025-07-11 14:28:14 +0700

    fix lỗi route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,26 @@
-
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/client/home/Home";
-import About from "../pages/client/about/About";
-import RequireAuth from "../guards/RequireAuth";
-import ClientLayout from "../layouts/ClientLayout";
-import AdminLayout from "../layouts/AdminLayout";
-import Dashboard from "../pages/admin/dashboard/Dashboard";
-import Setting from "../pages/admin/setting/Setting";
-import NotFound from "../pages/NotFound";
-import Register from "../pages/client/auth/Register";
-import Login from "../pages/client/auth/Login";
-import ForgotPassword from "../pages/client/auth/ForgotPassword";
-import ResetPassword from "../pages/client/auth/ResetPassword";
-import Contact from "../pages/client/contact/Contact";
-import PrivacyPolicy from "../pages/policy/PrivacyPolicy";
-import RefundPolicy from "../pages/policy/RefundPolicy";
-import ShippingPolicy from "../pages/policy/ShippingPolicy";
-import TermsOfService from "../pages/policy/TermsOfService";
-import PolicyForBuyers from "../pages/policy/PolicyForBuyers";
-import Collections from "../pages/client/collection/Collections";
-import CollectionDetail from "../pages/client/collection/CollectionDetail";
-import ProductDetail from "../pages/product/ProductDetail";
-import Checkout from "../pages/client/checkout/Checkout";
-
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/client/home/Home';
+import About from '../pages/client/about/About';
+import RequireAuth from '../guards/RequireAuth';
+import ClientLayout from '../layouts/ClientLayout';
+import AdminLayout from '../layouts/AdminLayout';
+import Dashboard from '../pages/admin/dashboard/Dashboard';
+import Setting from '../pages/admin/setting/Setting';
+import NotFound from '../pages/NotFound';
+import Register from '../pages/client/auth/Register';
+import Login from '../pages/client/auth/Login';
+import ForgotPassword from '../pages/client/auth/ForgotPassword';
+import ResetPassword from '../pages/client/auth/ResetPassword';
+import Contact from '../pages/client/contact/Contact';
+import PrivacyPolicy from '../pages/policy/PrivacyPolicy';
+import RefundPolicy from '../pages/policy/RefundPolicy';
+import ShippingPolicy from '../pages/policy/ShippingPolicy';
+import TermsOfService from '../pages/policy/TermsOfService';
+import PolicyForBuyers from '../pages/policy/PolicyForBuyers';
+import Collections from '../pages/client/collection/Collections';
+import CollectionDetail from '../pages/client/collection/CollectionDetail';
+import ProductDetail from '../pages/product/ProductDetail';
+import Checkout from '../pages/client/checkout/Checkout';
 
 const AppRoutes = () => {
     return (
@@ -34,7 +32,7 @@ const AppRoutes = () => {
                 <Route path="/contact" element={<Contact />} />
 
                 {/* policy routes */}
-                <Route path="/privacy-policy" element={<PrivacyPolicy />} />  
+                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                 <Route path="/refund-policy" element={<RefundPolicy />} />
                 <Route path="/shipping-policy" element={<ShippingPolicy />} />
                 <Route path="/terms-of-service" element={<TermsOfService />} />
@@ -55,24 +53,22 @@ const AppRoutes = () => {
 
                 {/* 404 */}
                 <Route path="*" element={<NotFound />} />
-              </Route>
+            </Route>
 
-              {/* checkout route */}
-              <Route path="/checkouts" element={<Checkout />} />
+            {/* checkout route */}
+            <Route path="/checkouts" element={<Checkout />} />
 
-              {/* admin routes */}
-              <Route
+            {/* admin routes */}
+            <Route
                 path="/admin"
                 element={
-                  <RequireAuth>
-                    <AdminLayout />
-                  </RequireAuth>
+                    <RequireAuth>
+                        <AdminLayout />
+                    </RequireAuth>
                 }
-              >
+            >
                 <Route index element={<Dashboard />} />
                 <Route path="settings" element={<Setting />} />
-              </Route>
-
             </Route>
 
             {/* admin routes */}
